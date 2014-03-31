c35a302e7fd39c695b9dab1a7c5a421e9076bff0
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2014-03-31 10:45:36 -0300

    add

---
+++ public/instagram4you/instagram/instagram.config.php
@@ -0,0 +1,9 @@
+<?php
+// Setup class
+  $instagram = new Instagram(array(
+    'apiKey'      => 'bfe85b5e42344c98b58886cf8cfce096',
+    'apiSecret'   => 'afb94af2809148bcad9c84bea67ec72b',
+    'apiCallback' => 'http://localhost/instagram4you/instagram/success.php' // must point to success.php
+  ));
+
+?>
