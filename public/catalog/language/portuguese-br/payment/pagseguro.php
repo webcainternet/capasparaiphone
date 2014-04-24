80e0f05f8ef9dd76119496583b4325a7e655a0d6
Fernando Mendes <fernando.mendes@webca.com.br>
2014-04-24 17:34:31 -0300

    VQMod - PagSeguro Lightbox

--- public/catalog/language/portuguese-br/payment/pagseguro.php
+++ public/catalog/language/portuguese-br/payment/pagseguro.php
@@ -1,7 +1,7 @@
 <?php
 $_['text_title'] 		= 'PagSeguro';
-$_['text_information'] 	= 'Ao clicar no botão abaixo, você será redirecionado para o site do PagSeguro para efeturar o pagamento. Ao concluir o pagamento você retornará automaticamente para a loja.';
+$_['text_information'] 	= 'Ao clicar no botão abaixo, ira abrir o Pagamento dentro da loja e com a segurança do Pagseguro.';
 $_['text_wait']         = 'Por favor, aguarde!';
-$_['button_confirm_pagseguro'] = 'Confirmar e ir para o PagSeguro';
+$_['button_confirm_pagseguro'] = 'Confirmar e abrir o Pagamento';
 $_['text_extra_amount']	= 'Frete e Taxas';
 ?>
