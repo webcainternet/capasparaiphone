bdbbdb44754a750edba232f75c6cd5609250f675
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2014-05-07 03:20:50 -0300

    inicial pub

---
+++ public/admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.capasparaiphone.com.br/admin/');
+define('HTTP_CATALOG', 'http://www.capasparaiphone.com.br/');
+define('HTTP_IMAGE', 'http://www.capasparaiphone.com.br/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.capasparaiphone.com.br/admin/');
+define('HTTPS_CATALOG', 'https://www.capasparaiphone.com.br/');
+define('HTTPS_IMAGE', 'https://www.capasparaiphone.com.br/image/');
+
+// DIR
+define('DIR_APPLICATION', '/srv/httpd/capasparaiphone.com.br/public/admin/');
+define('DIR_SYSTEM', '/srv/httpd/capasparaiphone.com.br/public/system/');
+define('DIR_DATABASE', '/srv/httpd/capasparaiphone.com.br/public/system/database/');
+define('DIR_LANGUAGE', '/srv/httpd/capasparaiphone.com.br/public/admin/language/');
+define('DIR_TEMPLATE', '/srv/httpd/capasparaiphone.com.br/public/admin/view/template/');
+define('DIR_CONFIG', '/srv/httpd/capasparaiphone.com.br/public/system/config/');
+define('DIR_IMAGE', '/srv/httpd/capasparaiphone.com.br/public/image/');
+define('DIR_CACHE', '/srv/httpd/capasparaiphone.com.br/public/system/cache/');
+define('DIR_DOWNLOAD', '/srv/httpd/capasparaiphone.com.br/public/download/');
+define('DIR_LOGS', '/srv/httpd/capasparaiphone.com.br/public/system/logs/');
+define('DIR_CATALOG', '/srv/httpd/capasparaiphone.com.br/public/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'capasparaiphone.cewvuskapqij.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'capasparaiphone');
+define('DB_PASSWORD', 'djuHsy26dt');
+define('DB_DATABASE', 'capasparaiphone');
+define('DB_PREFIX', 'sea_');
+?>
